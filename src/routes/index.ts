a0e85ac3417e9e4c706a4a487f099de1b729bb47
Andrei25i <andreiiosif2504@gmail.com>
2026-08-06 15:13:21 +0300

    feat: Implement routes for getting the ads

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import categoriesRouter from "./categories.routes";
 import authRouter from "./auth.routes";
+import adsRouter from "./ads.routes";
 
 const router = Router();
 
-// router.use("/ads", adsRouter);
+router.use("/ads", adsRouter);
 // router.use("/user", userRouter);
 router.use("/categories", categoriesRouter);
 router.use("/auth", authRouter);
---
+++ src/types/ads.types.ts
@@ -0,0 +1,9 @@
+export type GetAdsFilters = {
+  search?: string;
+  category?: string;
+  minPrice?: string;
+  maxPrice?: string;
+  city?: string;
+  userId?: string;
+  sort?: string;
+};
