6101d284d8750ff52d45336ff6af83530f871bd3
Antonescu Andrei-Iosif <78093165+Andrei25i@users.noreply.github.com>
2026-08-09 10:50:01 +0300

    feat: Implement get user route, controller and service (#9)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,12 @@ import categoriesRouter from "./categories.routes";
 import authRouter from "./auth.routes";
 import adsRouter from "./ads.routes";
 import favoritesRouter from "./favorites.routes";
+import userRouter from "./user.routes";
 
 const router = Router();
 
 router.use("/ads", adsRouter);
-// router.use("/user", userRouter);
+router.use("/user", userRouter);
 router.use("/categories", categoriesRouter);
 router.use("/auth", authRouter);
 router.use("/favorites", favoritesRouter);
